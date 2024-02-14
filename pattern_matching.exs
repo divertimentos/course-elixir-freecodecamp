28bee6179a339f794640bcf50284b5e167b6f9d9
Guilherme Teixeira <24235344+ggteixeira@users.noreply.github.com>
2024-02-14 19:22:09 -0300

    Fundamentals of pattern matching

---
+++ pattern_matching.exs
@@ -0,0 +1,23 @@
+# one = 1
+# IO.puts(1 == one)
+# IO.puts(1 = one)
+#
+# name = "Octavarium"
+# IO.puts("Octavarium" == name)
+
+[a, a] = [1, 1]
+# IO.puts("a: #{a}")
+
+# [a, a] = [1, 2]
+# IO.puts("a: #{a}")
+
+[a, b] = [1, 2]
+IO.puts("a: #{a}")
+IO.puts("b: #{b}")
+
+# THAT'S PATTERN MATCHING!
+
+c = 1
+IO.puts("c equals to #{c}")
+c = 2
+IO.puts("Now c equals to #{c}")
