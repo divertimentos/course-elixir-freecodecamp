556124952decd226ed5a95f289b2d2b780a7677c
Guilherme Teixeira <24235344+ggteixeira@users.noreply.github.com>
2024-02-21 22:06:59 -0300

    Start new project tutorials and improves organization
    (I just hope I didn't mess everything)

---
+++ tutorials/lib/tutorials.ex
@@ -0,0 +1,18 @@
+defmodule Tutorials do
+  @moduledoc """
+  Documentation for `Tutorials`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Tutorials.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
