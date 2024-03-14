e21ceab274ea9cc593daf455971ca6040acbbeb6
Guilherme Teixeira <24235344+ggteixeira@users.noreply.github.com>
2024-03-14 16:28:29 -0300

    Adds Octallium's version

--- tutorials/lib/recursion/factorial.ex
+++ tutorials/lib/recursion/factorial.ex
@@ -1,9 +1,17 @@
+# MY VERSION:
+
+# defmodule Tutorials.Recursion.Factorial do
+#   def factorial(num, acc \\ 1)
+#
+#   def factorial(1, acc), do: acc
+#
+#   def factorial(num, acc) do
+#     factorial(num - 1, acc * num)
+#   end
+# end
+
+# OCTALLIUM's VERSION:
 defmodule Tutorials.Recursion.Factorial do
-  def factorial(num, acc \\ 1)
-
-  def factorial(1, acc), do: acc
-
-  def factorial(num, acc) do
-    factorial(num - 1, acc * num)
-  end
+  def of(1), do: 1
+  def of(num), do: num * of(num - 1)
 end
